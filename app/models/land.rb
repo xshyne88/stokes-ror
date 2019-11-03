943b25be4abb0df1b7036fc5259db02e2b98e158
xshyne88 <chasehomedecor@gmail.com>
2019-11-03 10:19:52 -0500

    get application policy up, and validations on land spec

--- app/models/land.rb
+++ app/models/land.rb
@@ -1,2 +1,3 @@
 class Land < ApplicationRecord
+  validates :name, presence: true, uniqueness: true
 end
--- spec/models/land_spec.rb
+++ spec/models/land_spec.rb
@@ -1,5 +1,18 @@
 require 'rails_helper'
 
-RSpec.describe Land, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+describe Land, type: :model do
+  context "validations" do
+    it "is valid with valid attributes" do
+      expect(build(:land)).to be_valid
+    end
+
+    it { should validate_presence_of(:name) }
+
+    it "is not valid with a non-unique name" do
+      land = create(:land)
+      duplicate_land = build(:land, name: land.name)
+
+      expect(duplicate_land).not_to be_valid
+    end
+  end
 end
