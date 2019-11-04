4f428c8995f62809eac65c65edf1add08cdd11aa
xshyne88 <chasehomedecor@gmail.com>
2019-11-04 17:12:50 -0500

    query type

--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -1,7 +1,9 @@
 module Types
   class QueryType < Types::BaseObject
-    field :lands, resolver: Queries::LandsQuery
-    field :land, resolver: Queries::LandQuery
+    field :lands, resolver: Queries::Lands
+    field :land, resolver: Queries::Land
+    field :duty, resolver: Queries::Duty
+    field :duties, resolver: Queries::Duties
     # field :user, resolver: UserQuery
     # field :search_lands, resolver: SearchLandsQuery
   end
