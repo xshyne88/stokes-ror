e282852e366f28de35b0282e0c17792adb80526a
xshyne88 <chasehomedecor@gmail.com>
2019-11-07 21:48:58 -0500

    modify environment

--- config/environment.rb
+++ config/environment.rb
@@ -2,4 +2,12 @@
 require_relative 'application'
 
 # Initialize the Rails application.
+
+Rails.application.configure do
+  # Whitelist one hostname
+  config.hosts << "stokes-graveyard.cf"
+  # Whitelist a test domain
+  config.hosts << /application\.local\Z/
+end
+
 Rails.application.initialize!
