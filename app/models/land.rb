690e75dd8d94f713f33012be8f39538ba1d0678c
Chase Philips <chasehomedecor@gmail.com>
2019-11-14 10:41:15 -0500

    add has many through association

--- app/models/land.rb
+++ app/models/land.rb
@@ -2,6 +2,7 @@ class Land < ApplicationRecord
   validates :name, presence: true, uniqueness: true
 
   has_many :land_duties
+  has_many :duties, through: :land_duties
   has_many :user_land_duties
   has_many :notes, as: :noteable
 
