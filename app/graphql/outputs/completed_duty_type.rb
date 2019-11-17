648c582f60bf4ad2656ba66fc19f130a802f35a9
Chase Philips <chasehomedecor@gmail.com>
2019-11-17 18:10:52 -0500

    add last completed by

--- app/graphql/outputs/completed_duty_type.rb
+++ app/graphql/outputs/completed_duty_type.rb
@@ -7,6 +7,12 @@ module Outputs
     field :land_duty, Outputs::LandDutyType, null: true
     field :expires_at, GraphQL::Types::ISO8601DateTime, null: true
     field :expired, Boolean, null: true
+    field :last_completed_by, String, null: true
+
+    def last_completed_by
+      Loaders::AssociationLoader.for(CompletedDuty, :land_duty).load(@object) do |land_duty|
+      @object.last_completed_by
+    end
 
     def land_duty
       Loaders::AssociationLoader.for(CompletedDuty, :land_duty).load(@object)
