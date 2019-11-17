7a8af75cc996547dfb267d2452a9b86466a4aca1
Chase Philips <chasehomedecor@gmail.com>
2019-11-17 16:14:30 -0500

    add denoirmalized fields on completed duty

--- app/models/completed_duty.rb
+++ app/models/completed_duty.rb
@@ -1,31 +1,40 @@
 class CompletedDuty < ApplicationRecord
-  after_commit :update_land_duty_c, on: :create
-  after_commit :update_land_duty_ic, on: :destroy
+  after_commit :denormalize_land_duty_complete, on: :create
+  after_commit :denormalize_land_duty_incomplete, on: :destroy
 
   audited associated_with: :land_duty
 
   belongs_to :user
   belongs_to :land_duty
 
+  before_create :set_expires_at
+
+  scope :most_recent, -> {order('created_at DESC').limit(5)}
+
   has_many :notes, as: :noteable
 
-  def mark_complete
-    update(completed_at: DateTime.now)
-  end
-
-  def completed?
-    !completed_at.nil?
+  def mark_expired
+    update(expired: true)
   end
 
   private 
 
-  # TODO: where expired at has not occured
-  def update_land_duty_c
-    land_duty.update(status: :completed)
+  def set_expires_at
+    return if !expires_at.nil?
+
+    self.expires_at = DateTime.now + land_duty.estimated_days
+  end
+
+  def denormalize_land_duty_complete
+    land_duty.update(status: :completed, last_completed_by: audits.last.user)
+  end
+
+  def denormalize_land_duty_incomplete
+    return if self.expired?
+    land_duty.update(status: :incomplete, last_completed_by: last_completed_by)
   end 
 
-  # TODO: where expired at has not occured
-  def update_land_duty_ic
-    land_duty.update(status: :incomplete)
-  end 
+  def last_completed_by
+    land_duty.completed_duties.most_recent.first&.audits&.first&.user
+  end
 end
