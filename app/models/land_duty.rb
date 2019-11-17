60600a5102e4df6faeff8002c477707dc18284bc
Chase Philips <chasehomedecor@gmail.com>
2019-11-17 16:14:40 -0500

    take off useless fields on land duty

--- app/models/land_duty.rb
+++ app/models/land_duty.rb
@@ -5,20 +5,12 @@ class LandDuty < ApplicationRecord
   audited associated_with: :land
   has_associated_audits
 
-  has_many :completed_duties, dependent: :destroy
+  has_many :completed_duties
 
   belongs_to :land
   belongs_to :duty
 
-  before_create :set_expires_at
-
   private
-
-  def set_expires_at
-    return if !expires_at.nil?
-
-    self.expires_at = DateTime.now + estimated_days
-  end
 
   def update_land_last_completed
     land.update(last_completed_duty: duty.name)
